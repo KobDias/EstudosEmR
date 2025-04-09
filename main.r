26486aafebb2bb7af257a1beb7e3cac95c7b08fc
Sofhia Kobor Dias <41336556-kobdias@users.noreply.replit.com>
2025-04-09 23:02:10 +0000

    Criação do doc

--- main.r
+++ main.r
@@ -1,1 +1,33 @@
-print("hello world")
+mdc<-function(x,y){
+  if((x>y)==TRUE){
+    menor<-y
+  }else{
+    menor<-x
+  }
+  for(i in 1:menor){
+    if((x%%i==0)&(y%%i==0)){
+      mdc<-i
+    }
+  }
+  return(mdc)
+}
+
+mmc<-function(y,x){
+  if((y>x)==TRUE){
+    maior<-y
+  }else{
+    maior<-x
+  }
+  while(TRUE){
+    if((maior%%x==0) & (maior%%y==0)){
+      mmc=maior
+      break
+    }
+    maior=maior+1
+  }
+  return(mmc)
+}
+
+{a=as.numeric(readline("digite o primeiro numero:"))
+b=as.numeric(readline("digite o segundo numero:"))
+paste("O MMC é", mmc(a,b), "O MDC é", mdc(a,b))}
