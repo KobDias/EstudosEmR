3bf4e2f6c327b0f9b7bbb215403c563b3ecf24a1
Sofhia Kobor Dias <sofhia.dias@etec.sp.gov.br>
2025-04-09 23:23:17 +0000

    alteração e organização

---
+++ teste.r
@@ -0,0 +1,12 @@
+vetorA <- c(1,3,2,4)
+vetorB <-c(1,4,5,8)
+
+vetorC<-c(vetorA, vetorB)
+paste(vetorC)
+
+matrixA<-matrix(c(2,4,5,6,7,10), nrow=2, ncol=3, byrow=TRUE)
+
+matrixB<-matrix(c(2,5,70,6,6,10), nrow=2, ncol=3, byrow=TRUE)
+
+paste(matrixB*matrixA) # multiplicação de matriz!
+
